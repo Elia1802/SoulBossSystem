44a4c544cc3ffcf0d6285e96afd50cec644e7465
Elia1802 <timlucas.huth@outlook.de>
2023-01-30 20:46:19 +0100

    Edit Comments

--- src/main/java/de/elia/soulboss/fight/BossFight.java
+++ src/main/java/de/elia/soulboss/fight/BossFight.java
@@ -8,6 +8,12 @@ import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * @author Elia
+ * @version 1.0
+ * @since 1.0
+ * @description This is BossFight class.
+ */
 public class BossFight {
 
   private final Location location;
@@ -16,27 +22,57 @@ public class BossFight {
   private Boss boss;
   private final BossFightManager bossFightManager = new BossFightManager();
   private final Register.Configuration configuration = new Register.Configuration();
+  private final CustomMessages message = new CustomMessages();
+  public static BossFight bossFight;
 
+  /**
+   * @author Elia
+   * @version 1.0
+   * @since 1.0
+   * @description This is constructor create a new Bossfight and start him.
+   * @param player Requrieres a {@link Player}.
+   * @param location Requieres a {@link Location}.
+   * @param key Requieres a key as {@link String}.
+   */
   public BossFight(Player player, Location location, String key){
     this.player = player;
     this.location = location;
     this.key = key;
     startFight();
+    bossFight = this;
   }
 
+  /**
+   * @author Elia
+   * @version 1.0
+   * @since 1.0
+   * @description This is constructor create a new Bossfight and start him.
+   * @param player Requrieres a {@link Player}.
+   * @param location Requieres a {@link Location}.
+   */
   public BossFight(Player player, Location location){
     this(player, location, "680035753");
   }
 
+  /**
+   * @author Elia
+   * @version 1.0
+   * @since 1.0
+   * @description This is methode removed the boss with a specify reaseon.
+   */
   public void despawnBoss(Entity.RemovalReason reason){
     this.boss.remove(reason);
   }
 
+  /**
+   * @author Elia
+   * @version 1.0
+   * @since 1.0
+   * @description This is methode start the Bossfight
+   */
   public void startFight(){
-    CustomMessages message = new CustomMessages();
-    boss = new Boss(location);
     if (player == null)return;
-    message.messageWithPrefix(player, message.green("Der Boss Kampf wird in kürze beginnen"));
+    boss = new Boss(location);
     bossFightManager.setPlayerStart(player, true);
   }
 
@@ -47,39 +83,87 @@ public class BossFight {
    * @description This is methode stopped the Bossfight
    */
   public void stopFight(boolean removeFromList){
-    boss.remove(Entity.RemovalReason.KILLED);
+    this.despawnBoss(Entity.RemovalReason.KILLED);
     if (removeFromList) {
       bossFightManager.removePlayerStart(player);
     }
-    CustomMessages message = new CustomMessages();
     message.messageWithPrefix(player, message.gold("Der BossFight wurde beendet!"));
   }
 
+  /**
+   * @author Elia
+   * @version 1.0
+   * @since 1.0
+   * @description This is methode load the BossFight.
+   */
+  public static void loadBossFight(int ticks, Player player){
+    CustomMessages message = new CustomMessages();
+    message.messageWithPrefix(player, message.green("Der Boss Kampf wird in kürze beginnen"));
+    new Utils().start(ticks, player);
+  }
+
+  /**
+   * @author Elia
+   * @version 1.0
+   * @since 1.0
+   * @description This is methode gets the key.
+   */
   @NotNull
   public String key(){
     return key;
   }
 
+  /**
+   * @author Elia
+   * @version 1.0
+   * @since 1.0
+   * @description This is methode gets the boss.
+   */
   @NotNull
   public Boss boss() {
     return boss;
   }
 
+  /**
+   * @author Elia
+   * @version 1.0
+   * @since 1.0
+   * @description This is methode gets the BossFightManager.
+   */
   @NotNull
   public BossFightManager bossFightManager() {
     return bossFightManager;
   }
 
+  /**
+   * @author Elia
+   * @version 1.0
+   * @since 1.0
+   * @description This is methode gets the configuration.
+   */
   @NotNull
+  @Deprecated //Use the Register.Configuration Class to get the configuration.
   public Register.Configuration configuration() {
     return configuration;
   }
 
+  /**
+   * @author Elia
+   * @version 1.0
+   * @since 1.0
+   * @description This is methode gets the player.
+   */
   @NotNull
   public Player player() {
     return player;
   }
 
+  /**
+   * @author Elia
+   * @version 1.0
+   * @since 1.0
+   * @description This is methode gets the location.
+   */
   @NotNull
   public Location location() {
     return location;
