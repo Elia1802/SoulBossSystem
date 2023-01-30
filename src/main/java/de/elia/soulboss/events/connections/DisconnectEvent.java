3fd1ed58a7fdfa4197bde1c1a88b4ab7ba3ec339
Elia1802 <timlucas.huth@outlook.de>
2023-01-30 20:47:03 +0100

    Edit Comments

--- src/main/java/de/elia/soulboss/events/connections/DisconnectEvent.java
+++ src/main/java/de/elia/soulboss/events/connections/DisconnectEvent.java
@@ -7,8 +7,22 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * @author Elia
+ * @version 1.0
+ * @since 1.0
+ * @implements {@link Listener}
+ * @description This is the Listener for removing the boss if the player disconnected.
+ */
 public class DisconnectEvent implements Listener {
 
+  /**
+   * @author Elia
+   * @version 1.0
+   * @since 1.0
+   * @description This is the Event for removing the boss if the player disconnected.
+   * @param event Requires the {@link PlayerQuitEvent}
+   */
   @EventHandler
   public void onPlayerDisconnect(@NotNull PlayerQuitEvent event){
     BossFightManager bossFightManager = new BossFightManager();
