88a5217eb8906ed0c5a19c8dd7e055cb3b625869
Elia1802 <timlucas.huth@outlook.de>
2023-01-31 20:54:30 +0100

    Edit Comment

--- src/main/java/de/elia/soulboss/events/death/EntityDeathEvent.java
+++ src/main/java/de/elia/soulboss/events/death/EntityDeathEvent.java
@@ -14,7 +14,7 @@ import org.jetbrains.annotations.NotNull;
 import java.util.Collection;
 
 /**
- * @author Elia
+ * @author Zopnote, Elia
  * @version 1.0
  * @since 1.0
  * @implements {@link Listener}
