41f7590daa995895a13e9dcaa1daf436948aeb4b
Elia1802 <timlucas.huth@outlook.de>
2023-02-28 17:40:59 +0100

    Create UtilsLoader.java

---
+++ src/main/java/de/elia/soulboss/plugin/load/start/register/utils/UtilsLoader.java
@@ -0,0 +1,31 @@
+package de.elia.soulboss.plugin.load.start.register.utils;
+
+import de.elia.soulboss.block.BreakBlock;
+import de.elia.soulboss.messages.message.CustomMessages;
+import org.bukkit.NamespacedKey;
+import org.bukkit.plugin.Plugin;
+
+/**
+ * @author Elia
+ * @version 1.0
+ * @since 1.0
+ * @description This is the Register class to register all Utils or other things.
+ */
+public class UtilsLoader {
+
+  private final CustomMessages message = new CustomMessages();
+
+  /**
+   * @author Elia
+   * @version 1.0
+   * @since 1.0
+   * @description Register the Utils and other things
+   */
+  public void loadUtils(Plugin plugin){
+    String key = "680035753";
+    message.infoLog("load BreakTask with the NamespacedKey: " + key + "...");
+    new BreakBlock(plugin).breakTask(new NamespacedKey(plugin, key));
+    message.infoLog("BreakTasks loaded!");
+  }
+
+}
