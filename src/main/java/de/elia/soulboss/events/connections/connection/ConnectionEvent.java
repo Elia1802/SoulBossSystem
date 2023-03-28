8f427c74b007875b767ecc8f51981d1ff12ed34b
Elia1802 <timlucas.huth@outlook.de>
2023-03-28 20:27:16 +0200

    Edit Comments

--- src/main/java/de/elia/soulboss/events/connections/connection/ConnectionEvent.java
+++ src/main/java/de/elia/soulboss/events/connections/connection/ConnectionEvent.java
@@ -1,20 +1,34 @@
 package de.elia.soulboss.events.connections.connection;
 
-import de.elia.soulboss.SoulBoss;
-import de.elia.soulboss.messages.message.CustomMessages;
+import de.elia.CustomMessages;
+import de.elia.bossfightcreator.BossFightCreator;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * @author Elia
+ * @version 1.0
+ * @since 1.0
+ * @implements {@link Listener}
+ * @description Set the Bossfight status of the player to 0 (ready)
+ */
 public class ConnectionEvent implements Listener {
 
+  /**
+   * @author Elia
+   * @version 1.0
+   * @since 1.0
+   * @description Set the Bossfight status of the player to 0 (ready)
+   * @param event Requires the {@link PlayerJoinEvent}
+   */
   @EventHandler
   public void onConnection(@NotNull PlayerJoinEvent event){
     CustomMessages message = new CustomMessages();
     Player player = event.getPlayer();
-    var status = SoulBoss.playerStatusMap();
+    var status = BossFightCreator.playerStatusMap();
     if (status.get(player) == null) {
       status.put(player, 0);
     }else {
