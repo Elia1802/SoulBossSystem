a58e92a9c190a0352de460de7205dded8abd06cc
Elia1802 <timlucas.huth@outlook.de>
2023-03-28 20:24:02 +0200

    The function of this event is not aviable (comming soon)

--- src/main/java/de/elia/soulboss/events/connections/disconnection/DisconnectEvent.java
+++ src/main/java/de/elia/soulboss/events/connections/disconnection/DisconnectEvent.java
@@ -1,7 +1,6 @@
 package de.elia.soulboss.events.connections.disconnection;
 
-import de.elia.soulboss.SoulBoss;
-import de.elia.soulboss.commands.mob.SpawnMobCommand;
+import de.elia.bossfightcreator.BossFightCreator;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -12,9 +11,11 @@ import org.jetbrains.annotations.NotNull;
  * @author Elia
  * @version 1.0
  * @since 1.0
+ * @deprecated The function of this event is not aviable (comming soon)
  * @implements {@link Listener}
  * @description This is the Listener for removing the boss if the player disconnected.
  */
+@Deprecated
 public class DisconnectEvent implements Listener {
 
   /**
@@ -27,10 +28,10 @@ public class DisconnectEvent implements Listener {
   @EventHandler
   public void onDisconnection(@NotNull PlayerQuitEvent event){
     Player player = event.getPlayer();
-    var status = SoulBoss.playerStatusMap();
+    var status = BossFightCreator.playerStatusMap();
     if (status.get(player) == 1) {
-      if (SpawnMobCommand.getBossFight() == null)return;
-      SpawnMobCommand.getBossFight().stopFight(true);
+      //if (SpawnMobCommand.getBossFight() == null)return;
+      //SpawnMobCommand.getBossFight().stopFight();
       status.remove(player);
     }
   }
