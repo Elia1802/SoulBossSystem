007532aaee00e4200edf0db43c5a986334cf7c71
Elia1802 <timlucas.huth@outlook.de>
2023-03-31 16:35:25 +0200

    Change the log methodes and edit the key class to get the keys for the recipe and the boss

--- src/main/java/de/elia/soulboss/plugin/load/Load.java
+++ src/main/java/de/elia/soulboss/plugin/load/Load.java
@@ -1,27 +1,24 @@
 package de.elia.soulboss.plugin.load;
 
+import de.elia.Keys;
+import de.elia.soulboss.SoulBoss;
 import de.elia.soulboss.commands.help.HelpCommand;
 import de.elia.soulboss.commands.idea.IdeaCommand;
 import de.elia.soulboss.commands.items.ItemGiveCommand;
 import de.elia.soulboss.commands.plugin.PluginCommand;
 import de.elia.soulboss.commands.world.WorldCommand;
-import de.elia.soulboss.entity.mobs.boss.mob.BossDeathEvent;
 import de.elia.soulboss.events.achievements.bossfight.AchievementBossFightEvent;
 import de.elia.soulboss.events.achievements.bossfightzombieend.AchievementBossFightZombieEndEvent;
 import de.elia.soulboss.events.attacks.fire.AttackFireEvent;
 import de.elia.soulboss.events.attacks.strikelightning.AttackStrikeLightningEvent;
 import de.elia.soulboss.events.attacks.teleport.AttackTeleportEvent;
 import de.elia.soulboss.events.connections.connection.ConnectionEvent;
-import de.elia.soulboss.events.connections.disconnection.DisconnectEvent;
-import de.elia.soulboss.events.spawn.ZombieSpawnEvent;
-import de.elia.CustomMessages;
 import de.elia.soulboss.plugin.load.start.register.commands.CommandLoader;
 import de.elia.soulboss.plugin.load.start.register.configuation.ConfigurationLoader;
 import de.elia.soulboss.plugin.load.start.register.events.EventLoader;
 import de.elia.soulboss.plugin.load.start.register.recipes.RecipeLoader;
 import de.elia.soulboss.plugin.load.start.register.utils.UtilsLoader;
 import org.bukkit.Bukkit;
-import org.bukkit.NamespacedKey;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginManager;
 
@@ -33,7 +30,6 @@ import org.bukkit.plugin.PluginManager;
  */
 public class Load {
 
-  private final CustomMessages log = new CustomMessages();
   private final CommandLoader command = new CommandLoader();
   private final ConfigurationLoader configuration = new ConfigurationLoader();
   private final EventLoader event = new EventLoader();
@@ -49,37 +45,38 @@ public class Load {
    * @param pluginManager Requires a {@link PluginManager}
    */
   public void loadPlugin(Plugin plugin, PluginManager pluginManager){
-    log.infoLog("Load SoulBoss...");
-      log.infoLog("Load Commands...");
+    var log = SoulBoss.soulBoss().soulBossLogger();
+    log.logInfo("Load SoulBoss...");
+      log.logInfo("Load Commands...");
         command.loadCommand("bosshelp", new HelpCommand());
         command.loadCommand("mobidea", new IdeaCommand());
         command.loadCommand("bossgive", new ItemGiveCommand());
         //command.loadCommand("bossfight", new SpawnMobCommand());
         command.loadCommand("soulboss", new PluginCommand());
         command.loadCommand("tpworld", new WorldCommand());
-      log.infoLog("Commands loaded!");
-      log.infoLog("Load Events...");
+      log.logInfo("Commands loaded!");
+      log.logInfo("Load Events...");
         event.loadEvents(pluginManager, new AchievementBossFightEvent(), plugin);
         event.loadEvents(pluginManager, new AchievementBossFightZombieEndEvent(), plugin);
         event.loadEvents(pluginManager, new AttackFireEvent(), plugin);
         event.loadEvents(pluginManager, new AttackStrikeLightningEvent(), plugin);
         event.loadEvents(pluginManager, new AttackTeleportEvent(), plugin);
+        //event.loadEvents(pluginManager, new BossDeathEvent(), plugin);
         event.loadEvents(pluginManager, new ConnectionEvent(), plugin);
-        event.loadEvents(pluginManager, new DisconnectEvent(), plugin);
-        event.loadEvents(pluginManager, new BossDeathEvent(), plugin);
-        event.loadEvents(pluginManager, new ZombieSpawnEvent(), plugin);
-      log.infoLog("Events loaded!");
-      log.infoLog("Load Configurations...");
+        //event.loadEvents(pluginManager, new DisconnectEvent(), plugin); (Depraceted)
+        //event.loadEvents(pluginManager, new ZombieSpawnEvent(), plugin);
+      log.logInfo("Events loaded!");
+      log.logInfo("Load Configurations...");
         configuration.loadFiles(plugin);
-      log.infoLog("Configurations loaded!");
-      log.infoLog("load Utils...");
+      log.logInfo("Configurations loaded!");
+      log.logInfo("load Utils...");
         utils.loadUtils(plugin);
-      log.infoLog("Utils loaded!");
-      log.infoLog("load Recipes...");
+      log.logInfo("Utils loaded!");
+      log.logInfo("load Recipes...");
         recipeSpawnEgg.loadRecipe(plugin);
-      log.infoLog("Recipes loaded");
-    log.infoLog("All things loaded! Good Luck with this Plugin!");
-    log.infoLog("SoulBoss loaded!");
+      log.logInfo("Recipes loaded");
+    log.logInfo("All things loaded! Good Luck with this Plugin!");
+    log.logInfo("SoulBoss loaded!");
   }
 
   /**
@@ -91,61 +88,62 @@ public class Load {
    * @param pluginManager Requires a {@link PluginManager}
    */
   public void reloadPlugin(Plugin plugin, PluginManager pluginManager){
-    log.infoLog("Reload SoulBoss...");
-    log.infoLog("Remove Recipes...");
-      Bukkit.removeRecipe(new NamespacedKey(plugin, "v4j23hdu-df4e-36ta-r4sf-2n0c3n8cky9x"));
-    log.infoLog("Recipes Removed!");
-    log.infoLog("Save Configurations...");
+    var log = SoulBoss.soulBoss().soulBossLogger();
+    log.logInfo("Reload SoulBoss...");
+    log.logInfo("Remove Recipes...");
+      Bukkit.removeRecipe(Keys.RECIPE_KEY.key());
+    log.logInfo("Recipes Removed!");
+    log.logInfo("Save Configurations...");
       configuration.save(plugin);
-    log.infoLog("Configurations saved!");
-    log.infoLog("Remove Bosses...");
+    log.logInfo("Configurations saved!");
+    log.logInfo("Remove Bosses...");
     Bukkit.getServer().getWorld("world").getEntities().forEach((entity) ->{
-      if (entity.getPersistentDataContainer().has(new NamespacedKey(plugin, "680035753"))) {
+      if (entity.getPersistentDataContainer().has(Keys.ZOMBIE_KEY.key())) {
         entity.remove();
       }
     });
     Bukkit.getServer().getWorld("world_nether").getEntities().forEach((entity) ->{
-      if (entity.getPersistentDataContainer().has(new NamespacedKey(plugin, "680035753"))) {
+      if (entity.getPersistentDataContainer().has(Keys.ZOMBIE_KEY.key())) {
         entity.remove();
       }
     });
     Bukkit.getServer().getWorld("world_the_end").getEntities().forEach((entity) ->{
-      if (entity.getPersistentDataContainer().has(new NamespacedKey(plugin, "680035753"))) {
+      if (entity.getPersistentDataContainer().has(Keys.ZOMBIE_KEY.key())) {
         entity.remove();
       }
     });
     //configuration.playerRegisterStorage().clear();
-    log.infoLog("Bosses removed!");
-      log.infoLog("Load Commands...");
+    log.logInfo("Bosses removed!");
+      log.logInfo("Load Commands...");
         command.loadCommand("bosshelp", new HelpCommand());
         command.loadCommand("mobidea", new IdeaCommand());
         command.loadCommand("bossgive", new ItemGiveCommand());
         //command.loadCommand("bossfight", new SpawnMobCommand());
         command.loadCommand("soulboss", new PluginCommand());
         command.loadCommand("tpworld", new WorldCommand());
-      log.infoLog("Commands loaded!");
-      log.infoLog("Load Events...");
+      log.logInfo("Commands loaded!");
+      log.logInfo("Load Events...");
         event.loadEvents(pluginManager, new AchievementBossFightEvent(), plugin);
         event.loadEvents(pluginManager, new AchievementBossFightZombieEndEvent(), plugin);
         event.loadEvents(pluginManager, new AttackFireEvent(), plugin);
         event.loadEvents(pluginManager, new AttackStrikeLightningEvent(), plugin);
         event.loadEvents(pluginManager, new AttackTeleportEvent(), plugin);
+        //event.loadEvents(pluginManager, new BossDeathEvent(), plugin);
         event.loadEvents(pluginManager, new ConnectionEvent(), plugin);
-        event.loadEvents(pluginManager, new DisconnectEvent(), plugin);
-        event.loadEvents(pluginManager, new BossDeathEvent(), plugin);
-        event.loadEvents(pluginManager, new ZombieSpawnEvent(), plugin);
-      log.infoLog("Events loaded!");
-      log.infoLog("Load Configurations...");
+        //event.loadEvents(pluginManager, new DisconnectEvent(), plugin); (Depraceted)
+        //event.loadEvents(pluginManager, new ZombieSpawnEvent(), plugin);
+      log.logInfo("Events loaded!");
+      log.logInfo("Load Configurations...");
         configuration.loadFiles(plugin);
-      log.infoLog("Configurations loaded!");
-      log.infoLog("load Utils...");
+      log.logInfo("Configurations loaded!");
+      log.logInfo("load Utils...");
         utils.loadUtils(plugin);
-      log.infoLog("Utils loaded!");
-      log.infoLog("load Recipes...");
+      log.logInfo("Utils loaded!");
+      log.logInfo("load Recipes...");
         recipeSpawnEgg.loadRecipe(plugin);
-      log.infoLog("Recipes loaded");
-    log.infoLog("All things loaded! Good Luck with this Plugin!");
-    log.infoLog("SoulBoss reloaded!");
+      log.logInfo("Recipes loaded");
+    log.logInfo("All things loaded! Good Luck with this Plugin!");
+    log.logInfo("SoulBoss reloaded!");
   }
 
   /**
@@ -156,8 +154,9 @@ public class Load {
    * @param plugin Requires the Main class
    */
   public void reloadConfiguration(Plugin plugin){
-    log.infoLog("Load Configurations...");
+    var log = SoulBoss.soulBoss().soulBossLogger();
+    log.logInfo("Load Configurations...");
     configuration.loadFiles(plugin);
-    log.infoLog("Configurations loaded!");
+    log.logInfo("Configurations loaded!");
   }
 }
