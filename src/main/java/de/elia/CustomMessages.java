4cb46867149ba529b6472fb90f85f43ac64fefad
Elia1802 <timlucas.huth@outlook.de>
2023-03-31 16:11:51 +0200

    Deprecated log methodes because i have a PluginLogger

--- src/main/java/de/elia/CustomMessages.java
+++ src/main/java/de/elia/CustomMessages.java
@@ -9,6 +9,7 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
@@ -21,17 +22,18 @@ import java.util.logging.Logger;
 public class CustomMessages extends MessageBuilder {
 
   private final MiniMessage miniMessage = SoulMain.getMiniMessage();
-  private final Logger logger = Bukkit.getLogger();
+  @Deprecated private final Logger logger = Bukkit.getLogger();
   private final CustomPrefix prefix = new CustomPrefix();
 
   /**
    * @author Elia
    * @version 1.0
-   * @since 1.0
+   * @deprecated Use to log {@link  PluginLogger}
    * @description This Methode send an information in the console.
    * @param message Requires the log message.
    */
   @Override
+  @Deprecated //Use to log PluginLogger
   public void infoLog(String message) {
     this.logger.info(prefix.infoPrefix() + message);
   }
@@ -40,10 +42,12 @@ public class CustomMessages extends MessageBuilder {
    * @author Elia
    * @version 1.0
    * @since 1.0
+   * @deprecated Use to log {@link PluginLogger}
    * @description This Methode send a warning in the console.
    * @param message Requires the log message.
    */
   @Override
+  @Deprecated //Use to log PluginLogger
   public void warningLog(String message) {
     this.logger.warning(prefix.warningPrefix() + message);
   }
@@ -52,12 +56,43 @@ public class CustomMessages extends MessageBuilder {
    * @author Elia
    * @version 1.0
    * @since 1.0
+   * @deprecated Use to log {@link  PluginLogger}
    * @description This Methode send an error in the console.
    * @param message Requires the log message.
    */
   @Override
+  @Deprecated //Use to log PluginLogger
   public void errorLog(String message) {
     this.logger.severe(prefix.errorPrefix() + message);
+  }
+
+  /**
+   * @author Elia
+   * @version 1.0
+   * @since 1.0
+   * @deprecated Use to log {@link  PluginLogger}
+   * @description This Methode send a message with a specify level in the console.
+   * @param level Requires the level to log.
+   * @param message Requires the log message.
+   */
+  @Override
+  @Deprecated
+  public void log(Level level, String message){
+    this.logger.log(level, message);
+  }
+
+  /**
+   * @author Elia
+   * @version 1.0
+   * @since 1.0
+   * @deprecated Use to log {@link  PluginLogger}
+   * @description Gets the Logger.
+   */
+  @Override
+  @Deprecated
+  @NotNull
+  public Logger log(){
+    return this.logger;
   }
 
   /**
