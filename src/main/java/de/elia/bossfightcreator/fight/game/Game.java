36f065455e14928d13b58d152a234c48e4b90948
Elia1802 <timlucas.huth@outlook.de>
2023-03-31 16:30:05 +0200

    Create a new runnable fix bugs and edit the Keys class to get the key of the mob

--- src/main/java/de/elia/bossfightcreator/fight/game/Game.java
+++ src/main/java/de/elia/bossfightcreator/fight/game/Game.java
@@ -1,11 +1,12 @@
 package de.elia.bossfightcreator.fight.game;
 
-import de.elia.Main;
+import de.elia.Keys;
 import de.elia.bossfightcreator.Instances.Plugin;
 import de.elia.bossfightcreator.fight.game.maps.GameMaps;
 import de.elia.soulboss.entity.mobs.boss.mob.ZombieBoss;
+import de.elia.soulboss.utils.timers.StartTasks;
+import de.elia.soulboss.utils.timers.TimerUtils;
 import org.bukkit.Location;
-import org.bukkit.NamespacedKey;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
@@ -62,7 +63,7 @@ public class Game {
   public void killThis(@NotNull Player player){
     Collection<LivingEntity> entities = player.getLocation().getNearbyLivingEntities(5);
     for (Entity entity : entities){
-      if (entity.getPersistentDataContainer().has(new NamespacedKey(Main.main(), "680035753"))) {
+      if (entity.getPersistentDataContainer().has(Keys.ZOMBIE_KEY.key())) {
         entity.remove();
       }
     }
@@ -74,11 +75,65 @@ public class Game {
    * @since 1.0
    * @description End this Game
    */
-  public void removeGame(){
+  public void killGame(){
     var map = GameMaps.EXIST_GAME_OF_PLAYER;
-    if (map.containsKey(player.getUniqueId())) {
+    if (map.containsValue(this)) {
       this.killThis(player);
       map.remove(player.getUniqueId());
+      GameMaps.GAMES.remove(this);
     }
+  }
+
+  public void removeGame(){
+    var gameMap = GameMaps.EXIST_GAME_OF_PLAYER;
+    if (gameMap.containsValue(this)) {
+      new GameEndTimer().start(60*20, player, null);
+      gameMap.remove(player.getUniqueId());
+      GameMaps.GAMES.remove(this);
+      System.out.println("ERFOLGREICH");
+    }
+    new GameEndTimer().start(60*20, player, null);
+    gameMap.remove(player.getUniqueId());
+    GameMaps.GAMES.remove(this);
+    System.out.println("NICHT ERFOLGREICH");
+  }
+
+  public class GameEndTimer extends StartTasks {
+
+    public GameEndTimer(){
+
+    }
+
+    public void start(int time, Player player, Location location){
+      TimerUtils.countdownAndRun(time,
+        new Runnable() {
+          @Override
+          public void run() {
+
+          }
+        }
+      );
+      TimerUtils.countdownInterval(time,
+        new TimerUtils.TimeRunnable() {
+          @Override
+          public void run(int ticks) {
+            if (ticks % 20 == 0) {
+              int seconds = ticks/20; //Use this to show seconds
+              //run countdown
+            }
+          }
+        },
+        new Runnable() {
+          @Override
+          public void run() {
+            //if countdown 0 run this
+          }
+        }
+      );
+    }
+  }
+
+  private void timeMessage(){
+
   }
 }
