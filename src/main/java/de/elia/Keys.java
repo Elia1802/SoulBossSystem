a3a50b74065a05061e2fd3d0ebb123cc7b91e511
Elia1802 <timlucas.huth@outlook.de>
2023-04-13 17:53:43 +0200

    Commited Keys.java

--- src/main/java/de/elia/Keys.java
+++ src/main/java/de/elia/Keys.java
@@ -2,6 +2,12 @@ package de.elia;
 
 import org.bukkit.NamespacedKey;
 
+/**
+ * @author Elia
+ * @version 1.0
+ * @since 1.0
+ * @description Save all keys for the bosses and recipes
+ */
 public enum Keys {
 
   ZOMBIE_KEY(new NamespacedKey(Main.main(), "680035753")),
@@ -13,6 +19,13 @@ public enum Keys {
     this.key = key;
   }
 
+  /**
+   * @author Elia
+   * @version 1.0
+   * @since 1.0
+   * @description Gets the Key
+   * @return {@link NamespacedKey}
+   */
   public NamespacedKey key(){
     return this.key;
   }
