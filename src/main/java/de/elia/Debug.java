29bc0e5f664e17662321533d94b1cdb7db0aa5de
Elia1802 <timlucas.huth@outlook.de>
2023-04-13 17:54:09 +0200

    Create a class to debug

---
+++ src/main/java/de/elia/Debug.java
@@ -0,0 +1,22 @@
+package de.elia;
+
+/**
+ * @author Elia
+ * @version 1.0
+ * @since 1.0
+ * @description To Send Debug messages
+ */
+public class Debug {
+
+  /**
+   * @author Elia
+   * @version 1.0
+   * @since 1.0
+   * @description
+   * @param debug send a debug memssage
+   */
+  public static void debug(Object debug){
+    System.out.println("SOUL-DEBUG" + debug);
+  }
+
+}
