744f0d45b7e1bdbb7d0d420dec53d6572943de22
Elia1802 <timlucas.huth@outlook.de>
2023-06-14 16:00:37 +0200

    Edit bugfixes

--- src/main/java/de/elia/bossfightcreator/events/connect/ConnectionEvent.java
+++ src/main/java/de/elia/bossfightcreator/events/connect/ConnectionEvent.java
@@ -1,12 +1,13 @@
-package de.elia.soulboss.events.connections.connection;
+package de.elia.bossfightcreator.events.connect;
 
 import de.elia.systemclasses.messages.PluginMessages;
-import de.elia.bossfightcreator.BossFightCreator;
-import de.elia.soulboss.SoulBoss;
+import de.elia.bossfightcreator.BossFightCreatorMain;
+
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerLoginEvent;
+
 import org.jetbrains.annotations.NotNull;
 
 /**
@@ -23,16 +24,17 @@ public class ConnectionEvent implements Listener {
    * @version 1.0
    * @since 1.0
    * @description Set the Bossfight status of the player to 0 (ready)
-   * @param event Requires the {@link PlayerJoinEvent}
+   * @param event Requires the {@link PlayerLoginEvent}
    */
   @EventHandler
-  public void onConnection(@NotNull PlayerJoinEvent event){
+  public void onConnection(@NotNull PlayerLoginEvent event){
     PluginMessages message = new PluginMessages();
-    var log = SoulBoss.soulBoss().soulBossLogger();
+    var log = BossFightCreatorMain.bossFightCreator().bossFightCreatorLogger();
     Player player = event.getPlayer();
-    var status = BossFightCreator.playerStatusMap();
+    var status = BossFightCreatorMain.playerStatusMap();
     if (status.get(player) == null) {
       status.put(player, 0);
+      log.logInfo(player.getName() + " has status 0!");
     }else {
       log.logError("This player (" + player + ") has a exist Status!");
       log.logWarning("Status of the Player set to 0!");
