bf3a250b9852b00ef319af9b2d6b0c670e37aa2f
Elia1802 <timlucas.huth@outlook.de>
2023-12-03 10:56:00 +0100

    Test1

--- src/main/java/de/elia/Loader.java
+++ src/main/java/de/elia/Loader.java
@@ -13,5 +13,6 @@ public class Loader implements PluginLoader {
     String projectPath = System.getProperty("user.dir");
     String libraryPath = projectPath + "/plugins/";
     classpathBuilder.addLibrary(new JarLibrary(Path.of(libraryPath + "SoullLibrary-3.0.0.jar")));
+    short s = 9;
   }
 }
